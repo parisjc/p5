91edc600f2dc9eaea8d72d3e51786be71a29d0a5
Jean-christophe Paris <jckparis38@gmail.com>
2022-03-30 16:20:27 +0400

    Ajustement UploadFile

--- src/App/Controller/UploadFile.php
+++ src/App/Controller/UploadFile.php
@@ -7,29 +7,30 @@ use App\Repository\PostRepository;
 
 class UploadFile extends AbstractController
 {
+
     public function UpdatePost($post)
     {
-        var_dump($post);
-        if($_FILES['file']['error'] > 0 && $_FILES['file']['error'] != 4){
-            switch($_FILES['file']['error']){
+        $file = $_FILES;
+        if($file['file']['error'] > 0 && $file['file']['error'] != 4){
+            switch($file['file']['error']){
                 case UPLOAD_ERR_INI_SIZE:
-                    echo "Le fichier dépasse la taille maximale par PHP (".ini_get('upload_max_filesize').").";
+                    printf( "Le fichier dépasse la taille maximale par PHP (".ini_get('upload_max_filesize').").");
                     break;
                 case UPLOAD_ERR_FORM_SIZE:
-                    echo "Le fichier dépasse la taille maximale du formulaire(".ini_get('post_max_size').").";
+                    printf( "Le fichier dépasse la taille maximale du formulaire(".ini_get('post_max_size').").");
                     break;
                 case UPLOAD_ERR_PARTIAL:
-                    echo "Le fichier est transféré partiellement.";
+                    printf( "Le fichier est transféré partiellement.");
                     break;
                 case UPLOAD_ERR_NO_TMP_DIR:
                 case UPLOAD_ERR_CANT_WRITE:
                 case UPLOAD_ERR_EXTENSION:
-                    echo "Une erreur s'est produite lors de l'importation du fichier.";
+                printf( "Une erreur s'est produite lors de l'importation du fichier.");
                     break;
             }
         }
         /* Getting file name */
-        $filename = $_FILES['file']['name'];
+        $filename = $file['file']['name'];
 
         /* Location */
         $location = "./build/imgs/upload/".$filename;
@@ -39,11 +40,11 @@ class UploadFile extends AbstractController
             echo 0;
         }else{
             /* Upload file */
-            if(move_uploaded_file($_FILES['file']['tmp_name'], $location)){
+            if(move_uploaded_file($file['file']['tmp_name'], $location)){
                 PostRepository::setUpdatePostimg($post,$filename);
                 echo $location;
             }else{
-                echo 0;
+                printf( 0);
             }
         }
     }
